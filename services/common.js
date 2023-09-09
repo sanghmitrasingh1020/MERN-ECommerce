f5f900b76672d5f82d788d30768d4a6290ace729
Sanghmitra Singh <singhsanghmitra9@gmail.com>
2023-09-09 19:12:06 +0530

    test3

--- services/common.js
+++ services/common.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
-  host: "smtp.@gmail.com",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
